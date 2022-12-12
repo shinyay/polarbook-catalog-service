a8322eb7bf8c4b2ee6199b8d54311ec9e09940a9
shinyay <shinya.com@gmail.com>
2022-12-12 11:09:35 +0900

    Initial Commit

---
+++ src/main/kotlin/com/shinyay/polarbookshop/catalogservice/PolarbookCatalogServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.shinyay.polarbookshop.catalogservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolarbookCatalogServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PolarbookCatalogServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/shinyay/polarbookshop/catalogservice/PolarbookCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shinyay.polarbookshop.catalogservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PolarbookCatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
